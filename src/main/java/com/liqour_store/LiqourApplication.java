e8140ea339df1a66ef7307bf615a8bc2b9c98217
lmourine018 <lmourine8@gmail.com>
2024-03-12 12:37:21 +0300

    first commit

---
+++ src/main/java/com/liqour_store/LiqourApplication.java
@@ -0,0 +1,13 @@
+package com.liqour_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiqourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiqourApplication.class, args);
+	}
+
+}
