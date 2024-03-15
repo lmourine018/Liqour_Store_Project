1f8a1022f5a33230ee1dc44a4864be5f6fb6bcea
lmourine018 <lmourine8@gmail.com>
2024-03-15 09:42:09 +0300

    renaming

--- src/main/java/com/liqour_store/LiquorApplication.java
+++ src/main/java/com/liqour_store/LiquorApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiqourApplication {
+public class LiquorApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(LiqourApplication.class, args);
+		 SpringApplication.run(LiquorApplication.class, args);
 	}
 
 }
