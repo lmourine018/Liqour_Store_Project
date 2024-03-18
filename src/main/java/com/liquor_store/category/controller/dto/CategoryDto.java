be43af449c50deadd193a7d31dce21aae0fe9274
lmourine018 <lmourine8@gmail.com>
2024-03-18 14:05:33 +0300

    validations

--- src/main/java/com/liquor_store/category/controller/dto/CategoryDto.java
+++ src/main/java/com/liquor_store/category/controller/dto/CategoryDto.java
@@ -1,10 +1,13 @@
 package com.liquor_store.category.controller.dto;
 
+import jakarta.persistence.Column;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public class CategoryDto {
     private Integer id;
+    @Column(name = "name")
     private String name;
 }
--- src/main/java/com/liquor_store/products/entity/Product.java
+++ src/main/java/com/liquor_store/products/entity/Product.java
@@ -1,9 +1,13 @@
 package com.liquor_store.products.entity;
 
+import com.liquor_store.category.entity.Category;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
@@ -17,7 +21,9 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private Integer categoryId;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
     private String name;
     private String brand;
     private Double unit_cost;
